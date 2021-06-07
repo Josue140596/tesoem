4b34141182d3496d5fc03303650024283fc20a5d
DannyAceves <dannyaceves303@gmail.com>
2021-06-06 23:02:00 -0500

    REGISTRO MODIFICADO

--- database/migrations/2021_06_06_031204_registro.php
+++ database/migrations/2021_06_06_031204_registro.php
@@ -13,23 +13,23 @@ class Registro extends Migration
      */
     public function up()
     {
-        Schema::create('Registro',function(Blueprint $table){
+        Schema::create('Registro', function (Blueprint $table) {
             $table->id();
-            $table->string('tipo_de_usuario',8);
+            $table->string('tipo_de_usuario', 8);
             $table->string('nombre');
             $table->string('a_pat');
             $table->string('a_mat');
-            $table->integer('telefono',14);
+            $table->integer('telefono', 14);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('direccion',255);
-            $table->string('cod_postal',5);
+            $table->string('direccion', 255);
+            $table->string('cod_postal', 5);
             $table->string('localidad');
             $table->string('pais');
-            $table->string('semestre',2);
+            $table->string('semestre', 2);
             $table->date('fecha_nacimiento');
             $table->string('password');
-          });  
+        });
     }
 
     /**
