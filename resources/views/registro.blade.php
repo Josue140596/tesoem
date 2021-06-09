13f0d8e66024aa5de45bf0481ded68d44062f60b
DannyAceves <dannyaceves303@gmail.com>
2021-06-09 00:11:46 -0500

    Arreglo de datos

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -13,8 +13,8 @@
     <form action="/registro" method="POST" enctype="multipart/form-data">
         {{csrf_field()}}
         <input type="text" name="nombre" placeholder="Nombre"><br><br>
-        <input type="text" name="a_pat" placeholder="Apellido Paterno"><br><br>
-        <input type="text" name="a_mat" placeholder="Apellido Materno"><br><br>
+        <input type="text" name="apellido_p" placeholder="Apellido Paterno"><br><br>
+        <input type="text" name="apellido_m" placeholder="Apellido Materno"><br><br>
         <input type="date" name="fecha_nacimiento" placeholder="Nombre de la Calle"><br><br>
         <input type="text" name="direccion" placeholder="Direccion"><br><br>
         <input type="number" name="cod_postal" placeholder="Codigo Postal"><br><br>
@@ -26,14 +26,14 @@
 
 
         <select  name="id_tipo">
-            @foreach($typeusers as @$typeuser)
+            @foreach($typeusers as $typeuser)
                 <option value="{{$typeuser->id_tipo}}">
-                    {{$typeuser->tipo}}
+                    {{$typeuser->tipo}} 
                 </option>
             @endforeach
         </select><br><br>
 
-        <select name="carrera">
+        <select name="">
             <option selected>Selecciona tu carrera</option>
             <option value="1">Contabilidad</option>
             <option value="2">Licenciatura en gastronomia</option>
